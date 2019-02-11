686a39c22d52aa6f5211ff89b2d522a07f7c0159
Tuatoe <tuatoe.daynuah@gmail.com>
2019-02-10 21:08:37 -0600

    initial

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -5,11 +5,12 @@ import { Component, OnInit } from '@angular/core';
   templateUrl: './server.component.html',
   styleUrls: ['./server.component.css']
 })
-export class ServerComponent implements OnInit {
+export class ServerComponent{
 
-  constructor() { }
-
-  ngOnInit() {
+  serverId: number = 10;
+  serverStatus: string = 'offline';
+  getServerStatus(){
+    return this.serverStatus;
   }
 
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -2,12 +2,20 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-servers',
+  // template:'',
   templateUrl: './servers.component.html',
   styleUrls: ['./servers.component.css']
+ 
 })
 export class ServersComponent implements OnInit {
+  allowNewServer = false;
 
-  constructor() { }
+
+  constructor() {
+    setTimeout(()=> { 
+      this.allowNewServer = true;
+    }, 2000)
+   }
 
   ngOnInit() {
   }
